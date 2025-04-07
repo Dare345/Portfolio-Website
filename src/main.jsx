85dc485de2ecf08997b1a530145342fae9cd4b06
Dare345 <ydarren615@gmail.com>
2025-04-06 21:50:49 -0400

    change
    adding changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <HashRouter basename={"/Portfolio-Website/"}>
       <App />
     </HashRouter>
   </StrictMode>,
